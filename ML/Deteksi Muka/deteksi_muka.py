734d97f6b491151b847eb391aca8d4c597b86686
Reza Ali Nirwansyah <rezaalinirwansyah@gmail.com>
2024-05-29 00:29:28 +0700

    final program dataset

--- ML/Deteksi Muka/deteksi_muka.py
+++ ML/Deteksi Muka/deteksi_muka.py
@@ -1,8 +1,20 @@
 import cv2
 
+'''
+    TUtorial mengambil foto untuk dataset
+    siapkan folder di folder dataset
+        ML/Deteksi Muka/DataSet
+    copy path folder tersebut ke variabel path
+    jalankan program
+
+    tekan dan tahan tombol 'm' untuk menangkap gambar 1 per satu
+    tekan tombol 'c' untuk mengankgap gambar sekaligus
+'''
+
+
 # Inisialisasi classifier wajah
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
+path = "ML/Deteksi Muka/DataSet/Reza"
 # Fungsi untuk mengambil gambar
 def capture_images():
     cap = cv2.VideoCapture(0)  # Buka webcam
@@ -37,16 +49,18 @@ def capture_images():
             # Simpan gambar jika tombol 'c' ditekan
             if cv2.waitKey(1) & 0xFF == ord('c'):
                 tombol = not tombol
-                # count += 1
-                # cv2.imwrite(f'captured_{count}.jpg', face_crop)
-                # print(f"Image {count} captured!")
-                
-                # # Ambil 100 frame
-                # if count == 100:
-                #     break
+
             if tombol:
                 if count <= 100:
-                    cv2.imwrite(f'foto_{count}.jpg', face_crop)
+                    cv2.imwrite(path + f'foto_{count}.jpg', face_crop)
+                    print(f'foto_{count}.jpg captured')
+                    count += 1
+            
+            if cv2.waitKey(1) & 0xFF == ord('m'):
+                if count <= 100:
+                    cv2.imwrite(path + f'foto_{count}.jpg', face_crop)
+                    print(f'foto_{count}.jpg captured')
+                    count += 1
             
 
         # Keluar dari loop jika tombol 'q' ditekan
